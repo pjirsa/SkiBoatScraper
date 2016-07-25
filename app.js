632d25be75d21f71fe2003a02a1fab988286f85c
Phil Jirsa <pjirsa@gmail.com>
2016-07-25 15:03:54 -0500

    Add filtering to remove blank rows

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var fs = require('fs');
 var tabletojson = require('tabletojson');
+var _ = require('underscore');
 
 var inputData = fs.readFileSync('./boats.html', 'utf8');
 
 var tables = tabletojson.convert(inputData);
 var boatList = tables[0];
-fs.writeFile("./boats.json", JSON.stringify(boatList), 'utf8');
+var results = _.filter(boatList, function(data){
+    return data["POST ID"].length > 0;
+});
+
+fs.writeFile("./boats.json", JSON.stringify(results), 'utf8');
